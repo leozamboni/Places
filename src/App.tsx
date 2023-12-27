184df55f7fd7e53d37bd04787d1817beb4d93dd8
leozamboni <leonardonunes169@gmail.com>
2023-12-27 18:25:13 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Suspense, useRef } from 'react';
 import './App.css';
-import { GridItem, ChakraProvider, Flex, Grid, Text } from '@chakra-ui/react'
+import { GridItem, ChakraProvider, Flex, Grid, Text, Box, Image, Button, Center } from '@chakra-ui/react'
 import { Environment, Gltf, OrbitControls, PerspectiveCamera, useGLTF } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { Model } from './model';
 
 
 function App() {
+  
+
   return (
     <ChakraProvider>
       <Grid
@@ -15,21 +17,39 @@ function App() {
         templateColumns='repeat(4, 1fr)'
         gap={1}
       >
-        <GridItem colSpan={2} bg='papayawhip' pl='44' pt='44'>
-          <Text fontSize='8xl' fontFamily='Adobe Text Pro Semibold'>Places</Text>
+        <GridItem colSpan={2} bg='yellow.200' pl='44' pt='44'>
+          <Text fontSize='8xl' fontFamily='Adobe Text Pro Semibold'>Places;</Text>
           <Text fontSize='6xl' fontFamily='Adobe Text Pro'>Walk and explore the world in photogrammetry!</Text>
         </GridItem>
         <GridItem rowSpan={2} colSpan={2} >
-          
+
           <Canvas >
             <Suspense fallback={null}>
               <Model />
+              <PerspectiveCamera makeDefault position={[-5, -6, -33]} fov={45} />
               <OrbitControls />
-              
             </Suspense>
           </Canvas>
         </GridItem>
-        <GridItem colSpan={2} bg='' />
+        <GridItem bg='blackalpha-200' colSpan={2} position='relative'>
+          <Box position='absolute' top='50%' left='0' right='0'>
+            <Center>
+              <Button colorScheme='teal' variant='outline'>
+                Try
+              </Button>
+            </Center>
+          </Box>
+
+          <Box position='absolute' bottom='5' left='0' right='0'>
+            <Center>
+              <Text>Integrated with</Text>
+            </Center>
+            <Center>
+              <Image w='200px' src='/sketchfab-logo.png' alt='Dan Abramov' />
+            </Center>
+          </Box>
+
+        </GridItem>
       </Grid>
     </ChakraProvider>
 
