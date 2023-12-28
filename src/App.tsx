316417a3e61435a0a93867fef20f088d884e0fa0
leozamboni <leonardonunes169@gmail.com>
2023-12-28 17:54:21 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React, { Suspense, useRef } from 'react';
+import React, { useRef } from 'react';
 import './App.css'
 import { GridItem, ChakraProvider, Grid, Text, Box, Image, Button, Center } from '@chakra-ui/react'
-import { useKeyboardControls, KeyboardControls, PointerLockControls } from '@react-three/drei'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { Model } from './model';
-import { Vector3 } from 'three';
-import * as RAPIER from "@dimforge/rapier3d-compat"
-import { CapsuleCollider, MeshCollider, Physics, RigidBody, useRapier } from '@react-three/rapier'
-import { Player } from './components/player';
+import { PlacesRuntimeDemo } from './components/places-runtime-demo';
+
 
 function App() {
+  const pointerLockControlsRef = useRef()
   return (
     <ChakraProvider>
       <Grid
@@ -20,41 +16,13 @@ function App() {
       >
         <GridItem colSpan={2} bg='yellow.200' position='relative'>
           <Box position='absolute' left='15%' top='20%'>
-            <Text fontSize='8xl' fontFamily='Adobe Text Pro Semibold'>Places;</Text>
-            <Text fontSize='5xl' fontFamily='Adobe Text Pro'>Walk and explore the world in photogrammetry!</Text>
+            <Text fontSize='9xl' fontFamily='Adobe Text Pro Semibold' transform='scale(1,0.7)' >Places</Text>
+            <Text fontSize='6xl' fontFamily='Adobe Text Pro' mt='-70px' transform='scale(1,0.7)' >Walk and explore the world in photogrammetry!</Text>
           </Box>
 
         </GridItem>
-        <GridItem rowSpan={2} colSpan={2} >
-
-          <KeyboardControls
-            map={[
-              { name: "forward", keys: ["ArrowUp", "w", "W"] },
-              { name: "backward", keys: ["ArrowDown", "s", "S"] },
-              { name: "left", keys: ["ArrowLeft", "a", "A"] },
-              { name: "right", keys: ["ArrowRight", "d", "D"] },
-              { name: "jump", keys: ["Space"] },
-            ]}>
-            <Canvas >
-              <Suspense fallback={null}>
-                <Physics gravity={[0, -20, 0]}>
-
-
-                  <Player />
-                  <RigidBody type='fixed' colliders='hull'>
-                    <MeshCollider type={'trimesh'}>
-                      <Model />
-                    </MeshCollider>
-                  </RigidBody>
-
-                </Physics>
-                <PointerLockControls />
-                <ambientLight intensity={2} />
-                <pointLight intensity={0.8} position={[5, 0, 5]} />
-              </Suspense>
-            </Canvas>
-          </KeyboardControls>
-
+        <GridItem rowSpan={2} colSpan={2} position='relative'>
+          <PlacesRuntimeDemo pointerLockControlsRef={pointerLockControlsRef} />
         </GridItem>
         <GridItem bg='blackalpha-200' colSpan={2} position='relative'>
           <Box position='absolute' top='50%' left='0' right='0'>
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,22 +1,24 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { CapsuleCollider, RigidBody } from "@react-three/rapier";
+import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
 import React, { useRef } from "react";
 import { Vector3 } from "three";
+import * as RAPIER from "@dimforge/rapier3d-compat"
 
-
-export function Player() {
+export function Player({ position }: { position: [x:number,y:number,z:number] }) {
     const SPEED = 5
     const direction = new Vector3()
     const frontVector = new Vector3()
     const sideVector = new Vector3()
     const ref = useRef() as any
+    const rapier = useRapier()
     const [, get] = useKeyboardControls()
     const { camera } = useThree()
+    camera.rotation.set(0,9.8,0)
 
     useFrame(() => {
         const { forward, backward, left, right, jump } = get()
-        const velocity = ref.current?.linvel() ?? 10
+        const velocity = ref.current.linvel()
 
         // update camera
         let p = ref.current.translation()
@@ -29,12 +31,14 @@ export function Player() {
         ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
 
         // jumping
-        const grounded = p.y <= -7
-        if (jump && grounded) ref.current.setLinvel({ x: 0, y: 5.5, z: 0 })
+        const world = rapier.world.raw()
+        const ray = world.castRay(new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 }), 1,false)
+        const grounded = ray && ray.collider && Math.abs(ray.toi) < 1
+        if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 })
     })
 
     return (
-        <RigidBody ref={ref} colliders={false} mass={1} type="dynamic" position={[0, 10, 0]} enabledRotations={[false, false, false]}>
+        <RigidBody ref={ref} colliders={false} mass={1} type="dynamic" position={position} enabledRotations={[false, false, false]}>
             <CapsuleCollider args={[0.70, 0.3]} />
         </RigidBody>
     )
